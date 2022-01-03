ed5ff8d1700f03b7683425552450e790ac74d495
Cyletix <1016120209@qq.com>
2022-01-04 02:00:21 +0800

    增加干员的实装时间的爬取

--- get_op_info.py
+++ get_op_info.py
@@ -7,10 +7,11 @@ global df
 
 
 def get_info(html):#info
-    local_column=['干员','英文']
-    name = html.xpath('//*[@id="charname"]')[0].text  #干员
+    local_column=['干员','英文','上线时间']
+    codename = html.xpath('//*[@id="charname"]')[0].text  #干员
     english = html.xpath('//*[@id="charname-en"]')[0].text  #英文
-    return local_column,[name, english]
+    join_time = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="上线时间\n"]/tbody/tr[2]/td')[0].text.strip('\n').replace('年','-').replace('月','-').replace('日','')
+    return local_column,[codename, english, join_time]
 
 def get_attribute(html):#attribute
     local_column=[
@@ -24,43 +25,32 @@ def get_attribute(html):#attribute
         '攻击间隔',
         '再部署时间',]
     try:
-        deploy_pos = html.xpath('//*[@id="chartag2"]/div/a')[0].text  #部署位,近战/远程,新增词条
-        life         = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="生命上限\n"]/tbody/tr[th="生命上限\n"]/td[3]')[0].text[:-1]
-        attack       = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="攻击\n"]/tbody/tr[th="攻击\n"]/td[3]')[0].text[:-1]
-        defence      = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="防御\n"]/tbody/tr[th="防御\n"]/td[3]')[0].text[:-1]
-        antimagic    = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="法术抗性\n"]/tbody/tr[th="法术抗性\n"]/td[3]')[0].text[:-1]
-        redeploy     = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="再部署时间\n"]/tbody/tr[th="初始部署费用\n"]/td[1]')[0].text[:-1]
-        cost         = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="初始部署费用\n"]/tbody/tr[th="初始部署费用\n"]/td[2]')[0].text[:-1]
-        block        = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="阻挡数\n"]/tbody/tr[th="阻挡数\n"]/td[1]')[0].text[:-1]
-        atk_interval = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="攻击间隔\n"]/tbody/tr[th="攻击间隔\n"]/td[2]')[0].text[:-1]
-        life_rely    = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="生命上限\n"]/tbody/tr[th="生命上限\n"]/td[5]')[0].text[:-1]
-        attack_rely  = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="攻击\n"]/tbody/tr[th="攻击\n"]/td[5]')[0].text[:-1]
-        defence_rely = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="防御\n"]/tbody/tr[th="防御\n"]/td[5]')[0].text[:-1]
+        deploy_pos   = html.xpath('//*[@id="chartag2"]/div/a')[0].text  #部署位,近战/远程,新增词条
+        life         = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="生命上限\n"]/tbody/tr[th="生命上限\n"]/td[3]')[0].text.strip('\n')
+        attack       = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="攻击\n"]/tbody/tr[th="攻击\n"]/td[3]')[0].text.strip('\n')
+        defence      = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="防御\n"]/tbody/tr[th="防御\n"]/td[3]')[0].text.strip('\n')
+        antimagic    = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="法术抗性\n"]/tbody/tr[th="法术抗性\n"]/td[3]')[0].text.strip('\n')
+        redeploy     = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="再部署时间\n"]/tbody/tr[th="初始部署费用\n"]/td[1]')[0].text.strip('\n')
+        cost         = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="初始部署费用\n"]/tbody/tr[th="初始部署费用\n"]/td[2]')[0].text.strip('\n')
+        block        = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="阻挡数\n"]/tbody/tr[th="阻挡数\n"]/td[1]')[0].text.strip('\n')
+        atk_interval = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="攻击间隔\n"]/tbody/tr[th="攻击间隔\n"]/td[2]')[0].text.strip('\n')
+        life_rely    = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="生命上限\n"]/tbody/tr[th="生命上限\n"]/td[5]')[0].text.strip('\n')
+        attack_rely  = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="攻击\n"]/tbody/tr[th="攻击\n"]/td[5]')[0].text.strip('\n')
+        defence_rely = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="防御\n"]/tbody/tr[th="防御\n"]/td[5]')[0].text.strip('\n')
     except(IndexError):
-        deploy_pos = html.xpath('//*[@id="chartag2"]/div/a')[0].text  #部署位,近战/远程,新增词条
-        block        = html.xpath('//*[@id="mf-section-3"]/table[1]/tbody/tr[2]/td[1]')[0].text[:-1]
-        cost         = html.xpath('//*[@id="mf-section-3"]/table[1]/tbody/tr[1]/td[2]')[0].text[:-1]
-        life         = html.xpath('//*[@id="mf-section-3"]/table[2]/tbody/tr[2]/td[3]')[0].text[:-1]
-        attack       = html.xpath('//*[@id="mf-section-3"]/table[2]/tbody/tr[3]/td[3]')[0].text[:-1]
-        defence      = html.xpath('//*[@id="mf-section-3"]/table[2]/tbody/tr[4]/td[3]')[0].text[:-1]
-        antimagic    = html.xpath('//*[@id="mf-section-3"]/table[2]/tbody/tr[5]/td[3]')[0].text[:-1]
-        atk_interval = html.xpath('//*[@id="mf-section-3"]/table[1]/tbody/tr[2]/td[2]')[0].text[:-2]
-        redeploy     = html.xpath('//*[@id="mf-section-3"]/table[1]/tbody/tr[1]/td[1]')[0].text[:-2]
-        life_rely    = html.xpath('//*[@id="mf-section-3"]/table[tbody/tr/th="生命上限\n"]/tbody/tr[th="生命上限\n"]/td[5]')[0].text[:-1]
-        attack_rely  = html.xpath('//*[@id="mf-section-3"]/table[tbody/tr/th="攻击\n"]/tbody/tr[th="攻击\n"]/td[5]')[0].text[:-1]
-        defence_rely = html.xpath('//*[@id="mf-section-3"]/table[tbody/tr/th="防御\n"]/tbody/tr[th="防御\n"]/td[5]')[0].text[:-1]
+        deploy_pos = life = attack = defence = antimagic = redeploy = cost = block = atk_interval = life_rely = attack_rely = defence_rely = None
     if life=='——':
-        life         = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="生命上限\n"]/tbody/tr[th="生命上限\n"]/td[2]')[0].text[:-1]
-        attack       = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="攻击\n"]/tbody/tr[th="攻击\n"]/td[2]')[0].text[:-1]
-        defence      = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="防御\n"]/tbody/tr[th="防御\n"]/td[2]')[0].text[:-1]
-        antimagic    = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="法术抗性\n"]/tbody/tr[th="法术抗性\n"]/td[2]')[0].text[:-1]
+        life         = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="生命上限\n"]/tbody/tr[th="生命上限\n"]/td[2]')[0].text.strip('\n')
+        attack       = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="攻击\n"]/tbody/tr[th="攻击\n"]/td[2]')[0].text.strip('\n')
+        defence      = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="防御\n"]/tbody/tr[th="防御\n"]/td[2]')[0].text.strip('\n')
+        antimagic    = html.xpath('//*[@id="mw-content-text"]/div/table[tbody/tr/th="法术抗性\n"]/tbody/tr[th="法术抗性\n"]/td[2]')[0].text.strip('\n')
     #计算信赖
     if life_rely!='':
         life=str(int(life)+int(life_rely))
     if attack_rely!='':
         attack=str(int(attack)+int(attack_rely))
     if defence_rely!='':
-        defence=str(int(defence)+int(defence_rely))    
+        defence=str(int(defence)+int(defence_rely))
     return local_column,[deploy_pos,block,cost,life,attack,defence,antimagic,atk_interval,redeploy]
 def get_skill(html):#skill
     local_column=[
@@ -74,38 +64,23 @@ def get_skill(html):#skill
         '技能三消耗',
         '技能三持续',]
     try:
-        s1_start  = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能1")]/following-sibling::table/tbody/tr[9]/td[3]')[0].text[:-1]#技能1
-        s1_cost   = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能1")]/following-sibling::table/tbody/tr[9]/td[4]')[0].text[:-1]#技能1
-        s1_durate = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能1")]/following-sibling::table/tbody/tr[9]/td[5]')[0].text[:-1]#技能1
+        s1_start  = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能1")]/following-sibling::table/tbody/tr[9]/td[3]')[0].text.strip('\n')#技能1
+        s1_cost   = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能1")]/following-sibling::table/tbody/tr[9]/td[4]')[0].text.strip('\n')#技能1
+        s1_durate = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能1")]/following-sibling::table/tbody/tr[9]/td[5]')[0].text.strip('\n')#技能1
     except(IndexError):
-        try:
-            s1_start  = html.xpath('//*[@id="mf-section-7"]/table[1]/tbody/tr[9]/td[3]')[0].text[:-1]
-            s1_cost   = html.xpath('//*[@id="mf-section-7"]/table[1]/tbody/tr[9]/td[4]')[0].text[:-1]
-            s1_durate = html.xpath('//*[@id="mf-section-7"]/table[1]/tbody/tr[9]/td[5]')[0].text[:-1]
-        except:#空缺情况
-            s1_start=s1_cost=s1_durate=None
+        s1_start=s1_cost=s1_durate=None
     try:
-        s2_start  = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能2")]/following-sibling::table/tbody/tr[9]/td[3]')[0].text[:-1]#技能2
-        s2_cost   = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能2")]/following-sibling::table/tbody/tr[9]/td[4]')[0].text[:-1]#技能2
-        s2_durate = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能2")]/following-sibling::table/tbody/tr[9]/td[5]')[0].text[:-1]#技能2
+        s2_start  = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能2")]/following-sibling::table/tbody/tr[9]/td[3]')[0].text.strip('\n')#技能2
+        s2_cost   = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能2")]/following-sibling::table/tbody/tr[9]/td[4]')[0].text.strip('\n')#技能2
+        s2_durate = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能2")]/following-sibling::table/tbody/tr[9]/td[5]')[0].text.strip('\n')#技能2
     except(IndexError):
-        try:
-            s2_start  = html.xpath('//*[@id="mf-section-7"]/table[2]/tbody/tr[9]/td[3]')[0].text[:-1]
-            s2_cost   = html.xpath('//*[@id="mf-section-7"]/table[2]/tbody/tr[9]/td[4]')[0].text[:-1]
-            s2_durate = html.xpath('//*[@id="mf-section-7"]/table[2]/tbody/tr[9]/td[5]')[0].text[:-1]
-        except:#空缺情况
-            s2_start=s2_cost=s2_durate=None
+        s2_start=s2_cost=s2_durate=None
     try:
-        s3_start  = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能3")]/following-sibling::table/tbody/tr[9]/td[3]')[0].text[:-1]#技能3
-        s3_cost   = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能3")]/following-sibling::table/tbody/tr[9]/td[4]')[0].text[:-1]#技能3
-        s3_durate = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能3")]/following-sibling::table/tbody/tr[9]/td[5]')[0].text[:-1]#技能3
-    except(IndexError):
-        try:
-            s3_start  = html.xpath('//*[@id="mf-section-7"]/table[3]/tbody/tr[9]/td[3]')[0].text[:-1]
-            s3_cost   = html.xpath('//*[@id="mf-section-7"]/table[3]/tbody/tr[9]/td[4]')[0].text[:-1]
-            s3_durate = html.xpath('//*[@id="mf-section-7"]/table[3]/tbody/tr[9]/td[5]')[0].text[:-1]
-        except(IndexError):#空缺情况
-            s3_start=s3_cost=s3_durate=None
+        s3_start  = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能3")]/following-sibling::table/tbody/tr[9]/td[3]')[0].text.strip('\n')#技能3
+        s3_cost   = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能3")]/following-sibling::table/tbody/tr[9]/td[4]')[0].text.strip('\n')#技能3
+        s3_durate = html.xpath('//*[@id="mw-content-text"]/div/p[contains(b,"技能3")]/following-sibling::table/tbody/tr[9]/td[5]')[0].text.strip('\n')#技能3
+    except(IndexError):#空缺情况
+        s3_start=s3_cost=s3_durate=None
     return local_column,[s1_start,s1_cost,s1_durate,s2_start,s2_cost,s2_durate,s3_start,s3_cost,s3_durate]
 
 def get_potential(html):
@@ -117,18 +92,12 @@ def get_potential(html):
         '潜能6',]
     ptt=[]
     try:
-        ptt.append(html.xpath('//*[@id="mw-content-text"]/div/h2[span[@id="潜能提升"]]/following-sibling::table/tbody/tr[2]/td[1]')[0].text[:-1])
-        ptt.append(html.xpath('//*[@id="mw-content-text"]/div/h2[span[@id="潜能提升"]]/following-sibling::table/tbody/tr[2]/td[2]')[0].text[:-1])
-        ptt.append(html.xpath('//*[@id="mw-content-text"]/div/h2[span[@id="潜能提升"]]/following-sibling::table/tbody/tr[2]/td[3]')[0].text[:-1])
-        ptt.append(html.xpath('//*[@id="mw-content-text"]/div/h2[span[@id="潜能提升"]]/following-sibling::table/tbody/tr[2]/td[4]')[0].text[:-1])
-        ptt.append(html.xpath('//*[@id="mw-content-text"]/div/h2[span[@id="潜能提升"]]/following-sibling::table/tbody/tr[2]/td[5]')[0].text[:-1])
-    except(IndexError):
-        ptt.append(html.xpath('//*[@id="mf-section-6"]/table/tbody/tr[1]/td')[0].text[:-1])
-        ptt.append(html.xpath('//*[@id="mf-section-6"]/table/tbody/tr[2]/td')[0].text[:-1])
-        ptt.append(html.xpath('//*[@id="mf-section-6"]/table/tbody/tr[3]/td')[0].text[:-1])
-        ptt.append(html.xpath('//*[@id="mf-section-6"]/table/tbody/tr[4]/td')[0].text[:-1])
-        ptt.append(html.xpath('//*[@id="mf-section-6"]/table/tbody/tr[5]/td')[0].text[:-1])
-    except(TypeError):
+        ptt.append(html.xpath('//*[@id="mw-content-text"]/div/h2[span[@id="潜能提升"]]/following-sibling::table/tbody/tr[2]/td[1]')[0].text.strip('\n'))
+        ptt.append(html.xpath('//*[@id="mw-content-text"]/div/h2[span[@id="潜能提升"]]/following-sibling::table/tbody/tr[2]/td[2]')[0].text.strip('\n'))
+        ptt.append(html.xpath('//*[@id="mw-content-text"]/div/h2[span[@id="潜能提升"]]/following-sibling::table/tbody/tr[2]/td[3]')[0].text.strip('\n'))
+        ptt.append(html.xpath('//*[@id="mw-content-text"]/div/h2[span[@id="潜能提升"]]/following-sibling::table/tbody/tr[2]/td[4]')[0].text.strip('\n'))
+        ptt.append(html.xpath('//*[@id="mw-content-text"]/div/h2[span[@id="潜能提升"]]/following-sibling::table/tbody/tr[2]/td[5]')[0].text.strip('\n'))
+    except:
         ptt=[None,None,None,None,None]
     return local_column,ptt
 
@@ -137,6 +106,7 @@ def assemble(name, html):#没加信赖，后面还得改
     if 'df' not in globals():
         columns=get_info(html)[0]+get_attribute(html)[0]+get_skill(html)[0]+get_potential(html)[0]
         df = pd.DataFrame(columns=columns)
+        
     info      = get_info      (html)[1]
     attribute = get_attribute (html)[1]
     # rely      = get_rely      (html)[1]
@@ -148,16 +118,21 @@ def assemble(name, html):#没加信赖，后面还得改
     return df
 
 if __name__ == '__main__':
-    # name_list = ['灵知', '极光', '耶拉']
-    name_list =pgq.pg_query('干员')
-    name_list[name_list.index('阿米娅（近卫）')]='阿米娅(近卫)'
+    # codename_list=['刻刀']#单项调试
+    codename_list =pgq.pg_query('干员')
+    codename_list[codename_list.index('阿米娅（近卫）')]='阿米娅(近卫)'
+    # codename_list=codename_list[codename_list.index('安赛尔'):]#从此位置开始
     save_path='杂项/wiki爬取干员信息6.xlsx'
-    for name in name_list:
-        cache_path='cache/html_cache_{}.html'.format(name)#缓存路径
+    for codename in codename_list:
+        cache_path='cache/html_cache_{}.html'.format(codename)#缓存路径
         cache_flag=not os.path.isfile(cache_path)
+        if len(codename)<4:
+            prt_split='\t\t'
+        elif len(codename)>=4:
+            prt_split='\t'
         if cache_flag:#从网页爬取并保存为缓存
             wiki_url = 'https://prts.wiki/w/'
-            operator_url = wiki_url + name
+            operator_url = wiki_url + codename
             import time
             from requests import get
             from requests.models import ChunkedEncodingError
@@ -175,13 +150,14 @@ if __name__ == '__main__':
             html_cache = open(cache_path, 'w', encoding='utf-8')
             html_cache.write(res_text)
             html_cache.close()
-            print('{0}\t网页爬取成功({1}/{2})'.format(name,name_list.index(name)+1,len(name_list)))
+            print('{0}{1}网页爬取成功({2}/{3})'.format(codename,prt_split,codename_list.index(codename)+1,len(codename_list)))
             time.sleep(5)
         else:#从缓存读取
             with open(cache_path,'r',encoding='utf-8') as f:
                 res_text=f.read()
-            print('{0}\t缓存读取成功({1}/{2})'.format(name,name_list.index(name)+1,len(name_list)))
+            print('{0}{1}缓存读取成功({2}/{3})'.format(codename,prt_split,codename_list.index(codename)+1,len(codename_list)))
         html = etree.HTML(res_text)
-        df=assemble(name, html)
-    input('Press Enter to save data:\n')
+        df=assemble(codename, html)
+    df[['上线时间']] = df[['上线时间']].apply(pd.to_datetime)#修改df单列的数据类型
+    input('Press Enter to save data:')
     df.to_excel(save_path,index=False)
